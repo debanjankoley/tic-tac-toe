682f2d2e16b49184adc550a11857bcaaf7c1fc55
Debanjan Koley <81432461+debanjankoley@users.noreply.github.com>
2024-06-27 21:49:37 +0530

    Create a 2d array that will represent the state of the game board inside our gameBoard object

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('hello')
+const gameBoard = (function () {
+    const rows = 3;
+    const columns = 3
+    const board = [];
+    
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i][j] = '';
+        };
+    };
+
+    const getBoard = () => board;
+
+    return {getBoard};
+})();
